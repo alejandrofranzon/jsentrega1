4e80f7c8705d395e775292915da1664f7d4d089a
Franzon Alejandro <afranzon@bancociudad.com.ar>
2022-07-23 13:08:23 -0300

    inicio

---
+++ js/trabajoAF-js.js
@@ -0,0 +1,18 @@
+  
+
+console.log("Bienvenidos a mi Juego")
+console.warn("Tienes 5 vidas")
+console.error("pierdes la 5 vidas... GAME OVER")
+
+alert("Toque ACEPTAR par empezar")
+prompt("ingrese tu Nombre")
+let seleccion = confirm("comenzamos???")
+
+for (let i = 0; i < 5; i = i + 1) {
+    console.log("numero de vida", i)
+}
+
+alert("GAME OVER")
+prompt("volvemos a jugar??")
+
+
